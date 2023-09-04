9269bb2056a06ccb92801b82621c9edddc7e6a2b
fernas <fernas@gmail.com>
2023-09-04 11:25:11 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-50">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Parse your string</h2>
+      <p class="mt-2 text-center text-sm text-gray-600">
+        <span class="font-medium text-indigo-600 hover:text-indigo-500"> let's find out what your string hides  </span>
+      </p>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" @submit.prevent="decrypt">
+          <AlertComponent v-if="error" />
+          <div class="text-left">
+            <label for="string" class="block text-sm font-medium text-gray-700"> Your encrypted string </label>
+            <div class="mt-1">
+              <input v-model="string" id="string" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+            </div>
+          </div>
+          <div v-if="parsedData" class="bg-gray-100 p-4 rounded">
+            <p>Firstname: {{ parsedData.first_name }}</p>
+            <p>Lastname: {{ parsedData.last_name }}</p>
+            <p>ID: {{ parsedData.id }}</p>
+          </div>
+          <div>
+            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Get Data</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <FooterComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import FooterComponent from './components/FooterComponent'
+import AlertComponent from './components/AlertComponent'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { AlertComponent, FooterComponent },
+  setup() {
+    const string = ref('')
+    const parsedData = ref(null)
+    const error = ref(false)
+
+    const decrypt = () => {
+      error.value = false
+      const cleanedString = string.value.replace(/0+/g, '0').trim()
+      const parts = cleanedString.split('0').filter(part => part !== '')
+
+      if (parts.length === 3) {
+        const [first_name, last_name, id] = parts
+
+        parsedData.value = {
+          first_name,
+          last_name,
+          id
+        }
+      } else {
+        parsedData.value = null;
+        error.value = true
+      }
+    }
+
+    return {
+      error,
+      parsedData,
+      string,
+
+      decrypt
+    }
   }
 }
 </script>
---
+++ src/components/AlertComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-red-50 rounded-md p-4 text-left">
+        <div class="flex">
+            <div class="">
+                <h3 class="text-sm font-medium text-red-800">Something went wrong!</h3>
+                <div class="text-red-700 mt-2 text-sm">
+                    <p>Check that the string is correctly encrypted.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
